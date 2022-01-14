a4183a7cd6715c7ef85f007a2acb2a47e7ea6e6d
loren <lorenmichaelh@gmail.com>
2022-01-14 13:47:18 -0600

    commit tree 794ccd1d728f8d5fc3c73b0646c191ade73e0ab9

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+
+  // const lcWord = word.toLowerCase();
+  // const lcWordArr = [...lcWord];
+  // const lcWordArrRev = lcWordArr.reverse();
+  // const lcWordRev = lcWordArrRev.join('');
+  // if (lcWordRev === word) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+
+  const revWord = word.toLowerCase().split('').reverse().join('')
+  return revWord === word
 }
 
 /* 
@@ -7,7 +19,9 @@ function isPalindrome(word) {
 */
 
 /*
-  Add written explanation of your solution here
+  First, I wanted to make sure the word was all lowercase so that character comparison wouldn't be affected by case. Next, I looked for a way to reverse the string. The method I chose was to create an array from the word and use the .reverse() method. Then I used the .join() method to contatenate the characters back together into a string. Last, I wrote a test to give a truthy or falsey answer based on the comparison of the lowercase word vs the newly rewritten reversed word.
+
+  In optimization, I then combined the functions I used together and shortened my test to a single line.
 */
 
 // You can run `node index.js` to view these console logs
